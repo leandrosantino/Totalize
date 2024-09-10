70d0fd6559fd9d3ed84db2f331d863f76d1ad4c5
Eddie <106930824+Eddie-lima@users.noreply.github.com>
2024-09-10 02:36:56 -0300

    Banco de dados Pronto
    Criação da base de dados em SQLite para o projeto Totalize. Contém o schema da criação das tabelas, exemplos de INSERT e para funcionamento no caixa utilizei da função BEGIN TRANSACTION/ COMMIT.
    
    Pode necessitar de alteração em tipos de valores

---
+++ eddie/sql.sql
@@ -0,0 +1,77 @@
+-- Criação da tabela Produtos
+
+
+-- create table product (
+--  product_id integer primary key autoincrement,
+--  code integer(13) not null,
+--  description varchar(35) not null,
+--  price int not null);
+
+
+
+-- INSERT INTO product (code, description, price) 
+-- VALUES 
+-- (1234567890123, 'Sample Product 1', 1999),
+-- (2345678901234, 'Sample Product 2', 2999),
+-- (3456789012345, 'Sample Product 3', 3999);
+
+
+
+--Criação da tabela compras
+
+
+-- create table purchase (
+--     purchase_id integer primary key autoincrement,
+--     CPF char(11) not null,
+--     total_price integer not null,
+--     purchase_date text not null
+-- );
+
+
+
+-- INSERT INTO purchase (CPF, total_price, purchase_date) 
+-- VALUES 
+-- ('12345678901', 4999, '2024-09-10'),
+-- ('98765432100', 2999, '2024-09-11');
+
+
+-- Criação da tabela de produtos comprados
+
+
+
+-- CREATE TABLE purchased_products (
+--     product_id INTEGER,
+--     purchase_id INTEGER,
+--     amount INTEGER NOT NULL,
+--     PRIMARY KEY (product_id, purchase_id),
+--     FOREIGN KEY (product_id) REFERENCES product(product_id),
+--     FOREIGN KEY (purchase_id) REFERENCES purchase(purchase_id)
+-- );
+
+
+-- INSERT INTO purchased_products (product_id, purchase_id, amount) 
+-- VALUES 
+-- (1, 1, 2),
+-- (2, 1, 1),
+-- (3, 2, 1);
+
+
+-- Exemplo de como os comandos terão que ser no caixa
+
+
+-- BEGIN TRANSACTION;
+
+-- INSERT INTO product (code, description, price) 
+-- VALUES 
+-- (1234567890123, 'Sample Product 4', 1999);
+
+-- INSERT INTO purchase (CPF, total_price, purchase_date) 
+-- VALUES 
+-- ('12345678901', 1999, '2024-09-10');
+
+
+-- INSERT INTO purchased_products (product_id, purchase_id, amount) 
+-- VALUES 
+-- (4, 4, 4);
+
+-- COMMIT;
